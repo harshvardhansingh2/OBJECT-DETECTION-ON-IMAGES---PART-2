305d48935bd5b0ea8ed2365ccde422d13e77ab9c
harshvardhansingh2 <97771241+harshvardhansingh2@users.noreply.github.com>
2022-06-10 19:11:10 -0600

    Add files via upload

---
+++ door.js
@@ -0,0 +1,31 @@
+status = "";
+img = "";
+
+function preload()
+{
+    img = loadImage('door_knob_cabinet.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerText = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
